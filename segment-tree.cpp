f0ae557a504ce15f5f6998e895448c3100efdb2d
Jonathan Yuan <jy90023@gmail.com>
2022-02-07 09:36:40 -0500

    fixed macros on segment tree

--- segment-tree.cpp
+++ segment-tree.cpp
@@ -1,22 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#pragma GCC optimize "Ofast"
-#define ll long long
-#define ull unsigned long long
-#define MOD 1e9+7
-#define fi first
-#define se second
-#define INF 0x3f3f3f3f
 #define MAX 100005
-#define pb push_back
-#define LSB(S) (S & (-S))
-typedef pair<int, int> pii;
-typedef pair<int, pii> piii;
-typedef vector<int> vi;
-typedef vector<pii> vii;
-typedef vector<piii> viii;
-
 
 int N, M, arr[] = {-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, st[MAX*4], lazy[MAX*4];
 
